1136ed3473b5fa31e89ce76da8363f1af36c6e0f
C <chronocrusaderagain@yahoo.com>
2025-01-27 10:09:31 -0500

    Initial Commit

---
+++ c5gameTests/c5gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  c5gameTests.swift
+//  c5gameTests
+//
+//  Created by Cameron Slaughter on 1/27/25.
+//
+
+import Testing
+@testable import c5game
+
+struct c5gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
